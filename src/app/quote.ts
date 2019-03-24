82b59a6dc7d6e47ba494787ab146f2cca8dfb2e0
Jaccojwang <danolago@gmail.com>
2019-03-24 12:02:42 +0300

    Create upvote and downvote functions and attach them to buttons already created

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote{
   public showDetails: boolean;
-  constructor(public id:number, public saying: string, public author: string, public firstName: string,public lastName: string, public completeDate: Date) {
+  constructor(public id:number, public saying: string, public author: string, public firstName: string,public lastName: string, public completeDate: Date, public votes: number, public up: number, public down: number) {
     this.showDetails = false
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,15 +8,23 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'All that we are is the result of what we have thought.', 'Buddha', 'Ian', 'Ian', new Date(2018,6,19)),
-    new Quote(1, 'I have no special talent. I am only passionately curious.', 'Albert Einstein', 'Ian', 'Ian', new Date(2019,1,3)),
-    new Quote(1, 'If you judge people, you have no time to love them.', 'Mother Teresa', 'Ian', 'Ian', new Date(2018,10,7)),
-    new Quote(1, 'Wisely, and slow. They stumble that run fast.', 'William Shakespeare ', 'Ian', 'Ian', new Date(2018,5,6)),
-    new Quote(1, 'I have no special talent. I am only passionately curious.', 'Albert Einstein', 'Ian', 'Ian', new Date(2019,2,13))
+    new Quote(1, 'All that we are is the result of what we have thought.', 'Buddha', 'Ian', 'Ian', new Date(2018,6,19), 0, 0, 0),
+    new Quote(1, 'I have no special talent. I am only passionately curious.', 'Albert Einstein', 'Ian', 'Ian', new Date(2019,1,3), 0, 0, 0),
+    new Quote(1, 'If you judge people, you have no time to love them.', 'Mother Teresa', 'Ian', 'Ian', new Date(2018,10,7), 0, 0, 0),
+    new Quote(1, 'Wisely, and slow. They stumble that run fast.', 'William Shakespeare ', 'Ian', 'Ian', new Date(2018,5,6), 0, 0, 0),
+    new Quote(1, 'I have no special talent. I am only passionately curious.', 'Albert Einstein', 'Ian', 'Ian', new Date(2019,2,13), 0, 0, 0)
   ]
 
   toggleDetails(index: number) {
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  upVote(index: number) {
+    this.quotes[index].votes += 1;
+    this.quotes[index].up += 1;
+  }
+  downVote(index: number) {
+    this.quotes[index].votes -= 1;
+    this.quotes[index].down += 1;
   }
   deleteQuote(index: number){
       let toDelete = confirm(`Are you sure you want to delete this quote`)
@@ -29,8 +37,8 @@ export class QuoteComponent implements OnInit {
     quote.id = quoteLength+1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
+    quote.votes = 0;
   }
-
   constructor() { }
 
   ngOnInit() {
